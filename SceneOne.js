567c847203269a55977ad2ff37e5575981faf23c
Jonathan Taylor <jonathan@taylorhq.com>
2020-04-23 15:39:04 +0100

    update

--- SceneOne.js
+++ SceneOne.js
@@ -5,11 +5,13 @@ class SceneOne extends Phaser.Scene {
     }
 
     preload() {
-    
         for(var i =0; i < bosses.length; i++) {
-            this.load.atlas(bosses[i][0], 'assets/NPCs/'+bosses[i][0]+'spritesheet.png', 'assets/NPCs/'+bosses[i][0]+'sprites.json');
+            if(bosses[i][9] == 'SceneOne') {
+                this.load.atlas(bosses[i][0], 'assets/NPCs/'+bosses[i][0]+'spritesheet.png', 'assets/NPCs/'+bosses[i][0]+'sprites.json');
+            }
         }
         this.load.atlas('player', 'assets/Knight/spritesheet.png', 'assets/Knight/sprites.json');
+        this.load.atlas('playAtt', 'assets/Knight/paspritesheet.png', 'assets/Knight/pasprites.json');
         this.load.tilemapTiledJSON('map', 'assets/Map.json');
         this.load.image('tiles', 'assets/tiles.png', {frameWidth: 16, frameHeight: 16});
     }
@@ -33,7 +35,7 @@ class SceneOne extends Phaser.Scene {
         gameState.xloc = 250;
         gameState.yloc = 250;
         gameState.player = this.physics.add.sprite(gameState.xloc, gameState.yloc, 'player').setScale(1.2);
-        loadNPCs(this);
+        loadNPCs(this, 'SceneOne');
         var OnTop = map.createStaticLayer('OnTop', groundTiles, 0, 0);
     
     
@@ -44,15 +46,18 @@ class SceneOne extends Phaser.Scene {
         this.physics.world.setBounds(0, 0, 2000, 1500);
         
         gameState.player.setCollideWorldBounds(true);
+        gameState.player.hitpoints = 100;
         console.log("Player will not hit walls.")
-        this.cameras.main.startFollow(gameState.player);
+        this.cameras.main.startFollow(gameState.player, true);
         console.log("Camera is following Player.")
         gameState.cursors = this.input.keyboard.createCursorKeys();
     
-        const debugGraphics = this.add.graphics().setAlpha(0.75);
+        gameState.gameActive = true;
         
-    
-    
+        const debugGraphics = this.add.graphics().setAlpha(0);
+
+        gameState.playerDisplay = this.add.text(200, 200).setScrollFactor(0).setFontSize(10).setColor('#fcba03');;
+        
     
         clipped.renderDebug(debugGraphics, {
             tileColor: null, // Color of non-colliding tiles
@@ -62,9 +67,20 @@ class SceneOne extends Phaser.Scene {
         
         
         this.physics.add.collider(gameState.player, clipped);
-        this.physics.add.overlap(npcId['boss1'], gameState.player)
-        //this.physics.add.collider(gamestate.player, npcId['boss1'], attacked, null, game);
-    
+        this.physics.add.collider(npcId['boss2'], clipped);
+
+        this.physics.add.overlap(npcId['boss2'], gameState.player, () => {
+            gameState.player.hitpoints -= npcId['boss2'].attPower;
+            console.log(gameState.player.hitpoints)
+        });
+
+        this.anims.create({
+            key: 'playAtt',
+            frames: this.anims.generateFrameNames('playAtt', {prefix: 'sprite0', start: 4, end: 8, zeroPad: 0}),
+            frameRate: 10,
+            repeat: -1
+        });
+
         this.anims.create({
             key: 'walk',
             frames: this.anims.generateFrameNames('player', {prefix: 'sprite', start: 1, end: 7, zeroPad: 0}),
@@ -77,25 +93,76 @@ class SceneOne extends Phaser.Scene {
             frameRate: 10,
             repeat: -1
         });
+
+        this.anims.create({
+            key: 'walkBoss',
+            frames: this.anims.generateFrameNames('boss2', {prefix: 'sprite', start: 1, end: 7, zeroPad: 0}),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'idleBoss',
+            frames: [{key: 'boss2', frame: 'sprite1'}],
+            frameRate: 10,
+            repeat: -1
+        });
+
+        npcId['boss2'].move = 0;
     }
 
-    update() {
-   
-        var cam = this.cameras.main;
-        if(gameState.cursors.space.isDown){
+update() {
+    /** ACTIONS IF GAME PLAYING */
+    if (gameState.gameActive == true) {
+            gameState.playerDisplay.setText([
+            'Health: '+gameState.player.hitpoints
+            ]);
+
+        /** IF OUT OF HITPOINTS */
+        if(gameState.player.hitpoints <= 0) {
+            gameState.gameActive = false;
+        }
+        var npc = npcId['boss2']
+        /** IF CLOSE TO BOSS, AND BOSS ALIVE, CHASE. IF NOT CLOSE, DON'T MOVE*/
+        if (isClose(npcId['boss2'], 100) && npc.alive == true) {
+            
+            chasePlayer(npcId['boss2'], 1, 150);
+            npcId['boss2'].move += 1;
+        } else if (npc.alive === true) {
+                if (npcId['boss2'].move >= 500){
+                    npc['boss2'].x = bosses[1][1];
+                    npc['boss2'].y = bosses[1][2];
+                }
+            npcId['boss2'].setVelocityX(0);
+            npcId['boss2'].setVelocityY(0);
         }
     
+        
+        /** Check if attacking... */
+        if (Phaser.Input.Keyboard.JustDown(gameState.cursors.space)) {
+            attackNpc(npcId['boss2']);
+        }
+
+        if (gameState.cursors.space.isDown) {
+            gameState.player.anims.play('playAtt', true);
+        }
+
+        /** Checks if boss is still alive... */
+        if(npcId['boss2'].alive == true && npcId['boss2'].health <= 0) {
+            npcId['boss2'].alive = false;
+            npcId['boss2'].destroy();
+        }
+    
+        /** Player Controls */
         if(gameState.cursors.left.isDown){
             gameState.player.flipX = true;
-            gameState.player.anims.play('walk', true);
+            gameState.player.anims.play('playAtt', true); 
             gameState.player.setVelocityX(-70)
             
         } else if (gameState.cursors.right.isDown) {
             gameState.player.flipX = false;
-            gameState.player.anims.play('walk', true);
+            gameState.player.anims.play('walk', true); 
             gameState.player.body.setVelocityX(70)
         } else {
-            gameState.player.anims.play('idle', true);
             gameState.player.body.setVelocityX(0)
         }
         if(gameState.cursors.up.isDown){
@@ -105,18 +172,28 @@ class SceneOne extends Phaser.Scene {
         } else if (gameState.cursors.down.isDown) {
             gameState.player.anims.play('walk', true);
             gameState.player.body.setVelocityY(70)
-            
         } else {
             gameState.player.body.setVelocityY(0)
         }
+        if(!gameState.cursors.left.isDown && !gameState.cursors.right.isDown && !gameState.cursors.up.isDown && !gameState.cursors.down.isDown ){
+            gameState.player.anims.play('idle', true);
+        }
+
+
+        /** LOSE CONDITION */
+    } else {
+        gameState.playerDisplay.setText([
+            'GAME OVER'
+        ]);
+        this.physics.pause();
+        gameState.player.body.setVelocityX(0)
+        gameState.player.body.setVelocityY(0)
+        /** Waits for Space Input */
+        if (Phaser.Input.Keyboard.JustDown(gameState.cursors.space)) {
+            this.scene.stop();
+            this.scene.start('MainScreen')
+        }
     }
-
-
-render() {
-
-    game.debug.cameraInfo(game.camera, 32, 32);
 }
-
-
 
 }
--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-var npcId = new Object();
-
-var bosses = [
-    ['boss1', 250, 250, 1.2, true, 150, 0.5, 20],
-    ['boss2', 450, 250, 1.2, true, 150, 0.5, 20],
-];
-
 class MainScreen extends Phaser.Scene {
     constructor() {
         super({key: 'MainScreen'})
@@ -12,25 +5,122 @@ class MainScreen extends Phaser.Scene {
 
     create() {
         this.add.text(350, 350, "Click to Start")
-
         this.input.on('pointerdown', () => {
             this.scene.stop();
             this.scene.start('SceneOne')
         });
     }
+}
 
+/* Checks whether player is in radius around Npc (parsed) */
+function isClose(npcId, range) {
+    if(npcId.alive == true) {
+    var maxX = npcId.x + range;
+    var minX = npcId.x - range;
+    var maxY = npcId.y + range;
+    var minY = npcId.y - range;
 
+    if(gameState.player.x >= minX && gameState.player.x <= maxX && gameState.player.y >= minY && gameState.player.y <= maxY) {
+    return true;
+    }
+}
+}
+
+function attackNpc(npcId) {
+        if(isClose(npcId, npcId.range)) {
+            npcId.health -= 2;
+            console.log(npcId.health)
+        } else if(npcId.alive == false) {
+                console.log("Npc Dead")
+        } else {
+            console.log("Not close enough")
+        }
 }
 
 
-function loadNPCs(game) {
+function chasePlayer(npcId, id, range) {
+    var playerX = gameState.player.x
+    var playerY = gameState.player.y
+
+    //If player larger
+        if(playerX > npcId.x) {
+            if(npcId.direction = 'left') {
+                npcId.flipX = true;
+            } else {
+                npcId.flipX = false;
+            }
+                npcId.setVelocityX(40)
+                npcId.play('walkBoss', true);
+                
+    //If player smaller
+        } else if (playerX < npcId.x) {
+            if(npcId.direction = 'right') {
+                npcId.flipX = true;
+            } else {
+                npcId.flipX = false;
+            }
+            npcId.setVelocityX(-40)
+            npcId.play('walkBoss', true);
+        }
+    //If player larger
+        if(playerY > npcId.y) {
+            if(npcId.direction = 'left') {
+                npcId.flipX = true;
+            } else {
+                npcId.flipX = false;
+            }
+            npcId.setVelocityY(40)
+            npcId.play('walkBoss', true);
+    //If player smaller
+        } else if (playerY < npcId.y) {
+            if(npcId.direction = 'right') {
+                npcId.flipX = true;
+            } else {
+                npcId.flipX = false;
+            }
+            npcId.setVelocityY(-40)
+            npcId.play('walkBoss', true);
+        }
+    }
+
+
+
+
+function render() {
+    game.debug.cameraInfo(game.camera, 32, 32);
+}
+/* Loads NPCs into scene and assigns attributes */
+/* [0:'Name', 1:XLoc, 2:YLoc, 3:Scale, 4:Range, 5:WayToFace, 6:SpriteFacing, 7:AttPower, 8:Health, 9:'Scene']  */
+function loadNPCs(game, scene) {
+
     for(var i =0; i < bosses.length; i++) {
-        npcId[bosses[i][0]] = game.physics.add.sprite(bosses[i][1], bosses[i][2], bosses[i][0]).setScale(bosses[i][3]);
-        npcId[bosses[i][0]].flipX = bosses[i][4];
-        npcId[bosses[i][0]].setCollideWorldBounds(true);
-        npcId[bosses[i][0]].attPower = bosses[i][6];
-        npcId[bosses[i][0]].health = bosses[i][7];
-        npcId[bosses[i][0]].setInteractive();
+        if(bosses[i][9] == scene) {
+            npcId[bosses[i][0]] = game.physics.add.sprite(bosses[i][1], bosses[i][2], bosses[i][0]).setScale(bosses[i][3]);
+            npcId[bosses[i][0]].setCollideWorldBounds(true);
+            npcId[bosses[i][0]].attPower = bosses[i][7];
+            npcId[bosses[i][0]].health = bosses[i][8];
+            npcId[bosses[i][0]].alive = true;
+            npcId[bosses[i][0]].range = bosses[i][4];
+            npcId[bosses[i][0]].setInteractive();
+            npcId[bosses[i][0]].direction = [bosses[i][5], bosses[i][6]];
+
+            if(bosses[i][5] == 'left') {
+                npcId[bosses[i][0]].direction = 'left';
+                if(bosses[i][6] == 'right') {
+                    npcId[bosses[i][0]].flipX = true;
+                   
+                } else {
+                    npcId[bosses[i][0]].flipX = false;
+                }
+            } else if(bosses[i][5] == 'right') {
+                
+                if(bosses[i][6] == 'left') {
+                    npcId[bosses[i][0]].flipX = true;
+                } else {
+                    npcId[bosses[i][0]].flipX = false;
+                }
+            }
+        }
     }
 }
 
@@ -47,7 +137,7 @@ const config = {
         arcade: {
             gravity: {y: 0},
             enableBody: true,
-            debug: true,
+            debug: false,
         }
     },
     scene: [MainScreen, SceneOne]
@@ -55,4 +145,10 @@ const config = {
 
 var map;
 var player;
+var npcId = new Object();
+/* ['Name', XLoc, YLoc, Scale, Range, WayToFace, SpriteFacing, Scale, Health, 'Scene']  */
+var bosses = [
+    ['boss1', 750, 300, 1.5, 70, 'right', 'right', 0.5, 20, 'SceneOne'],
+    ['boss2', 450, 250, 1.2, 50, 'left', 'right', 0.5, 20, 'SceneOne'],
+];
 const game = new Phaser.Game(config);
